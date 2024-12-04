ef257db900aab7bb057ee600e94d0ec82f1a8d9c
Antoine Goethals <antoine.gt@orange.fr>
2024-12-04 16:30:24 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MetroMap></MetroMap>
+</template>
+
+<script>
+import MetroMap from './components/MetroMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MetroMap
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+
+body {
+  background-color: #fffbdc;
+}
+
+</style>
